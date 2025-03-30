e97f3f3ee4012ae56ee88b9d80b6df576ff724b9
leeazyone <leejiwon317@naver.com>
2025-03-30 19:59:16 +0900

    eraser

--- app.js
+++ app.js
@@ -1,57 +1,77 @@
-const modebtn = document.getElementById('mode-btn')
+const modeBtn = document.getElementById('mode-btn')
+const destroyBtn = document.getElementById('destroy-btn')
+const eraserBtn = document.getElementById('eraser-btn')
 const colorOptions = Array.from(document.getElementsByClassName('color-option'))
 const color = document.getElementById('color')
-const linewidth = document.getElementById('line-width')
+const lineWidth = document.getElementById('line-width')
 const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d') // 그림을 그릴 때 사용
-canvas.width = 800
-canvas.height = 800
-ctx.linewidth = linewidth.value
+const ctx = canvas.getContext('2d')
+
+const CANVAS_WIDTH = 800
+const CANVAS_HEIGHT = 800
+
+canvas.width = CANVAS_WIDTH
+canvas.height = CANVAS_HEIGHT
+ctx.lineWidth = lineWidth.value
 let isPainting = false
 let isFilling = false
 
-function onMove(evnet) {
+function onMove(event) {
   if (isPainting) {
-    ctx.lineTo(evnet.offsetX, evnet.offsetY)
+    ctx.lineTo(event.offsetX, event.offsetY)
     ctx.stroke()
     return
   }
-  ctx.beginPath()
-  ctx.moveTo(evnet.offsetX, evnet.offsetY)
+  ctx.moveTo(event.offsetX, event.offsetY)
 }
-function startPainting(event) {
-  // 마우스 누르고 있을 때 그림 그리기
+function startPainting() {
   isPainting = true
 }
-function cancelPainting(event) {
+function cancelPainting() {
   isPainting = false
+  ctx.beginPath()
 }
 function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value
 }
+
 function onColorChange(event) {
   ctx.strokeStyle = event.target.value
   ctx.fillStyle = event.target.value
 }
+
 function onColorClick(event) {
   const colorValue = event.target.dataset.color
   ctx.strokeStyle = colorValue
   ctx.fillStyle = colorValue
-  color.value = colorValue //선택한 색깔 알려줌
+  color.value = colorValue
 }
+
 function onModeClick() {
   if (isFilling) {
     isFilling = false
-    modebtn.innerText = 'Fill'
+    modeBtn.innerText = 'Fill'
   } else {
     isFilling = true
-    modebtn.innerText = 'Draw'
+    modeBtn.innerText = 'Draw'
   }
 }
+
 function onCanvasClick() {
   if (isFilling) {
-    ctx.fillRect(0, 0, 800, 800)
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
   }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = 'white'
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = 'white'
+  isFilling = false
+  modeBtn.innerText = 'Fill'
 }
 
 canvas.addEventListener('mousemove', onMove)
@@ -59,8 +79,11 @@ canvas.addEventListener('mousedown', startPainting)
 canvas.addEventListener('mouseup', cancelPainting)
 canvas.addEventListener('mouseleave', cancelPainting)
 canvas.addEventListener('click', onCanvasClick)
-
-linewidth.addEventListener('change', onLineWidthChange)
+lineWidth.addEventListener('change', onLineWidthChange)
 color.addEventListener('change', onColorChange)
+
 colorOptions.forEach((color) => color.addEventListener('click', onColorClick))
-modebtn.addEventListener('click', onModeClick)
+
+modeBtn.addEventListener('click', onModeClick)
+destroyBtn.addEventListener('click', onDestroyClick)
+eraserBtn.addEventListener('click', onEraserClick)
