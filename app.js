d49cc7421b3fbc85fb45c3fe1485d86097b902ff
leeazyone <leejiwon317@naver.com>
2025-03-30 19:06:01 +0900

    Filling mode

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const modebtn = document.getElementById('mode-btn')
 const colorOptions = Array.from(document.getElementsByClassName('color-option'))
 const color = document.getElementById('color')
 const linewidth = document.getElementById('line-width')
@@ -7,6 +8,7 @@ canvas.width = 800
 canvas.height = 800
 ctx.linewidth = linewidth.value
 let isPainting = false
+let isFilling = false
 
 function onMove(evnet) {
   if (isPainting) {
@@ -17,7 +19,7 @@ function onMove(evnet) {
   ctx.beginPath()
   ctx.moveTo(evnet.offsetX, evnet.offsetY)
 }
-function onMouseDown(event) {
+function startPainting(event) {
   // 마우스 누르고 있을 때 그림 그리기
   isPainting = true
 }
@@ -35,14 +37,30 @@ function onColorClick(event) {
   const colorValue = event.target.dataset.color
   ctx.strokeStyle = colorValue
   ctx.fillStyle = colorValue
-  color.value = colorValue
+  color.value = colorValue //선택한 색깔 알려줌
+}
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false
+    modebtn.innerText = 'Fill'
+  } else {
+    isFilling = true
+    modebtn.innerText = 'Draw'
+  }
+}
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800)
+  }
 }
 
 canvas.addEventListener('mousemove', onMove)
-canvas.addEventListener('mousedown', onMouseDown)
+canvas.addEventListener('mousedown', startPainting)
 canvas.addEventListener('mouseup', cancelPainting)
 canvas.addEventListener('mouseleave', cancelPainting)
+canvas.addEventListener('click', onCanvasClick)
 
 linewidth.addEventListener('change', onLineWidthChange)
 color.addEventListener('change', onColorChange)
 colorOptions.forEach((color) => color.addEventListener('click', onColorClick))
+modebtn.addEventListener('click', onModeClick)
